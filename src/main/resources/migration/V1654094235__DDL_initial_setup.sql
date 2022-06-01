e01493beb9b7d3db705ca65a5b19fcd8c40bd4a7
Jeunn Hao <jeunnhao.chong@sicpa.com>
2022-06-01 22:48:39 +0800

    Add user flyway migration

---
+++ src/main/resources/migration/V1654094235__DDL_initial_setup.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users
+(
+    id         VARCHAR(50) PRIMARY KEY,
+    firstName  VARCHAR(255),
+    lastName   VARCHAR(255),
+    email      VARCHAR(255) UNIQUE NOT NULL,
+    created_on TIMESTAMP           NOT NULL
+);
