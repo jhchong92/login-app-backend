42e02f553f4fda621d2f1dcb2e021361fd556150
Jeunn Hao <jeunnhao.chong@sicpa.com>
2022-06-01 21:56:34 +0800

    Initial Spring boot commit

---
+++ src/main/java/com/jh/loginappbackend/LoginAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jh.loginappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAppBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LoginAppBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/jh/loginappbackend/LoginAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jh.loginappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAppBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
